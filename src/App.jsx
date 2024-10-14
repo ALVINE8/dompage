75c1d3a0fccea92ec4ab0220ecc95b742f3a4274
Alvine <tiekammerciel472@gmail.com>
2024-10-14 20:23:45 +0100

    nh

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const router= createBrowserRouter([
+  {path:'/',
+    element: <div>Accueil</div>
+  },
+  {path:'/Blog',
+    element: <div>Salut les gens duu blog</div>
+  }
+])
+function App() {
+  return (
+    <div>
+      <h1>bonjour </h1>
+    </div>
+  )
+   
+  
+}
+
+export default App;
