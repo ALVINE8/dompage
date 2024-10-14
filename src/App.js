3e309906479f4eaa9783026bb934046bca5f7cbe
Alvine <tiekammerciel472@gmail.com>
2024-10-14 11:19:28 +0100

    committwo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 
+const router= createBrowserRouter([
+  {path:'/',
+    element: <div>Accueil</div>
+  },
+  {path:'/Blog',
+    element: <div>Salut les gens duu blog</div>
+  }
+])
 function App() {
   return (
     <div>
